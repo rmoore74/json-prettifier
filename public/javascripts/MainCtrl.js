a05910efca82a4d221861e2368891f7c571a0c66
Roger Moore <me@rogermoore.io>
2016-07-20 14:08:18 +0100

    Added basic validation

--- public/javascripts/MainCtrl.js
+++ public/javascripts/MainCtrl.js
@@ -1,6 +1,18 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
     $scope.prettify = function() {
-        alert("test");
+        var jsonInput = document.getElementById('jsonInput');
+
+        alert(validate(jsonInput.value));
+    };
+
+    var validate = function(jsonInput) {
+        try {
+            JSON.parse(jsonInput);
+        } catch (e) {
+            return false;
+        }
+
+        return true;
     };
 });
